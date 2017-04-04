d0fc7c57f6c4d2e7475e849f01c57c471cc6e766
htkim <eptmxlsl1231@naver.com>
2017-04-04 09:31:13 +0900

    complete

--- hello/src/hello/helloworld.java
+++ hello/src/hello/helloworld.java
@@ -2,13 +2,9 @@ package hello;
 
 public class helloworld {
 	
-<<<<<<< HEAD
+
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-=======
-	public static void main(String[] args){
-		System.out.println("자고싶다 과제도못했는데");
->>>>>>> refs/heads/hotfix
 	}
 
 }
