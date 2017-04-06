96857551b5eb8764ca08d62788371ae2f2e854ca
soya-ht02 <eptmxlsl1231@naver.com>
2017-04-06 11:08:36 +0900

    Update PushPrint.java
    add bye

--- hello/src/hello/PushPrint.java
+++ hello/src/hello/PushPrint.java
@@ -4,7 +4,9 @@ public class PushPrint {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Add java file");
 		System.out.println("Hello .....");
+		System.out.println("Bye .....");
 
 	}
 
